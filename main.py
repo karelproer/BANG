d90cddb5311ab2bb921fb9ac2c5e4cbe8fd62b75
StellarMagpie <ihateithereplzzzhelp@gmail.com>
2025-11-08 12:04:54 +0100

    eurg

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parameters
-Nx, Ny = 300, 300
+Nx, Ny = 200,200
 Lx, Ly = 1.0, 1.0
 ax, ay = 0, 0.0
 dx, dy = Lx / Nx, Ly / Ny
@@ -18,7 +18,7 @@ X, Y = np.meshgrid(x, y, indexing='ij')
 
 # Beginwaarden
 rho = np.ones((Nx, Ny)) * rho0
-u = np.ones((Nx, Ny)) * 300
+u = np.ones((Nx, Ny)) * 30
 v = np.ones((Nx, Ny)) * 0
 e = np.ones((Nx, Ny)) * e0  # interne energie
 
@@ -30,10 +30,10 @@ Object_Mask = np.zeros((Nx, Ny), dtype=bool)
 #is_inside_y = (Y >= 0.4) & (Y <= 0.6)
 #Object_Mask = is_inside_x & is_inside_y
 
-m = 0.02
-p = 0.40
-t = 0.12
-a0, a1, a2, a3, a4 = 0.2969, -0.126, -0.3156, 0.2843, -0.1036
+M = 0.06 #Maximaal camber %
+P = 0.396 #max camber position in %
+T = 0.12 #max thickness
+a0, a1, a2, a3, a4 = 0.2969, -0.126, -0.3156, 0.2843, -0.1036 #standaard waarden
 
 # Scale + position wing
 x_wing = (X - 0.3) / 0.5   # move/scale in x
@@ -44,18 +44,18 @@ xch = np.clip(x_wing, 0.0, 1.0)
 
 # camber yc (piecewise) - vectorized
 yc = np.zeros_like(xch)
-left = xch <= p
+left = xch <= P
 right = ~left
-yc[left]  = (m / (p**2)) * (2*p*xch[left] - xch[left]**2)
-yc[right] = (m / ((1-p)**2)) * ((1 - 2*p) + 2*p*xch[right] - xch[right]**2)
+yc[left]  = (M / (P**2)) * (2*P*xch[left] - xch[left]**2)
+yc[right] = (M / ((1-P)**2)) * ((1 - 2*P) + 2*P*xch[right] - xch[right]**2)
 
 # derivative dyc/dx (piecewise)
 dyc_dx = np.zeros_like(xch)
-dyc_dx[left]  = (2*m / (p**2)) * (p - xch[left])
-dyc_dx[right] = (2*m / ((1-p)**2)) * (p - xch[right])
+dyc_dx[left]  = (2*M / (P**2)) * (P - xch[left])
+dyc_dx[right] = (2*M / ((1-P)**2)) * (P - xch[right])
 
 # thickness distribution 
-yt = (t / 0.2) * (
+yt = (T / 0.2) * (
     a0 * np.sqrt(xch) +
     a1 * xch +
     a2 * xch**2 +
