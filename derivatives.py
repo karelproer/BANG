36b5ec6f83282971b7e92cfc2d5eccb5a0ccfe18
karelproer <kthofman@xs4all.nl>
2025-12-28 14:51:26 +0100

    meer comments

--- derivatives.py
+++ derivatives.py
@@ -1,22 +1,28 @@
 import numpy as np
 
+# Afgelijde van waarde naar x-coordinaat
 def ddx(f, dx):
     d = np.zeros_like(f)
     d[..., 1:-1, :] = (f[..., 2:, :] - f[..., :-2, :]) / (2 * dx)
-    d[..., 0, :] = (f[..., 1, :] - f[..., 0, :]) / dx      # forward diff at left boundary
-    d[..., -1, :] = (f[..., -1, :] - f[..., -2, :]) / dx    # backward diff at right boundary
+    # Waarden aan de rand
+    d[..., 0, :] = (f[..., 1, :] - f[..., 0, :]) / dx
+    d[..., -1, :] = (f[..., -1, :] - f[..., -2, :]) / dx
     return d
 
+# Afgelijde van waarde naar y-coordinaat
 def ddy(f, dy):
     d = np.zeros_like(f)
     d[..., :, 1:-1] = (f[..., :, 2:] - f[..., :, :-2]) / (2 * dy)
-    d[..., :, 0] = (f[..., :, 1] - f[..., :, 0]) / dy       # forward diff at bottom boundary
-    d[..., :, -1] = (f[..., :, -1] - f[..., :, -2]) / dy    # backward diff at top boundary
+    # Waarden aan de rand
+    d[..., :, 0] = (f[..., :, 1] - f[..., :, 0]) / dy
+    d[..., :, -1] = (f[..., :, -1] - f[..., :, -2]) / dy 
     return d
 
+# tuple met beide afgeleiden
 def ddxy(f, dxy):
     return ddx(f, dxy[0]), ddy(f, dxy[1])
 
+# Laplacian van het veld f (hoe groot de waarden rond elk punt zijn vergeleken met dat punt zelf)
 def laplacian(f, dxy):
     grad_x, grad_y = ddx(f, dxy[0]), ddy(f, dxy[1])
     return ddx(grad_x, dxy[0]) + ddy(grad_y, dxy[1])
--- main.py
+++ main.py
@@ -6,49 +6,52 @@ from simulation import Simluation
 
 # Parameters
 Nx, Ny = 300,300 #pixels
-Lx, Ly = 1.0, 1.0
-ax, ay = 0, 0.0
+Lx, Ly = 1.0, 1.0 # groote van gesimuleerde gebied
 dx, dy = Lx / Nx, Ly / Ny
 dt = 0.00001 # in seconden
-Nt = 400000
-gamma = 1.4
-rho0, e0 = 1, 214_000
-rot_deg = 0
+Nt = 400000 # totaal aantal tijdstappen
+gamma = 1.4 # verhouding warmtecapaciteiten (parameter van lucht)
+rho0, e0 = 1, 214_000 # initieele dichtheid en temperatuur
+rot_deg = 0 #draaing van vluegel
 
 # Rooster
 x = np.linspace(0, Lx, Nx)
 y = np.linspace(0, Ly, Ny)
 X, Y = np.meshgrid(x, y, indexing='ij')
 
-Object_Mask= object_mask(Nx, Ny, Lx, Ly, rot_deg)
+Object_Mask= object_mask(Nx, Ny, Lx, Ly, rot_deg) # pixels waar de vluegel is
 
+# simulatie object
 sim = Simluation((Lx, Ly), (Nx, Ny), 1.4, 1, 214_000, (100, 0), Object_Mask)
 
-# Visualisatie setup
+# Visualisatie
 plt.ion()
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 5))
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 5)) # drie plaatjes
+
 im1 = ax1.imshow(sim.rho.T, origin='lower', extent=[0, Lx, 0, Ly], cmap='inferno', vmin=0.9999, vmax=1.0001)
 ax1.set_title("Density")
 plt.colorbar(im1, ax=ax1)
 im2 = ax2.imshow(sim.e.T, origin='lower', extent=[0, Lx, 0, Ly], cmap='inferno')
 ax2.set_title("Internal Energy")
 plt.colorbar(im2, ax=ax2)
-
 im3 = ax3.imshow(sim.u.T, origin='lower', extent=[0, Lx, 0, Ly], cmap='inferno', vmin=0, vmax=1)
 ax3.set_title("Speed")
 plt.colorbar(im3, ax=ax3)
-text = fig.text(0.5, 0.01, "", ha='center', va='bottom', fontsize=12, color='blue')
+
+text = fig.text(0.5, 0.01, "", ha='center', va='bottom', fontsize=12, color='blue') # text die lift en weerstand laat zien
 
 for n in range(Nt):
     sim.step(dt)
 
     t = n * dt
-    # Update plots elke paar stappen
+    # Update elke paar stappen
     if n % 10 == 0:
+        # automatisch aanpassen kleuren aan uiterste waarden
         im1.set_clim([sim.rho.min(), sim.rho.max()])
         im2.set_clim([sim.e.min(), sim.e.max()])
         im3.set_clim([sim.u.min(), sim.u.max()])
         
+        # plaatjes updaten met nieuwe waarden
         im1.set_data(sim.rho.T)
         ax1.set_title(f"Density at t = {t:.5f}s")
         im2.set_data(sim.e.T)
@@ -56,11 +59,13 @@ for n in range(Nt):
         im3.set_data(sim.u.T)
         ax3.set_title(f"Speed at t = {t:.5f}s")
         text.set_text(f"Drag = {sim.drag:.2f} N     Lift = {sim.lift:.2f} N")
+        
+        #wachten voor laten zien
         plt.pause(0.0001)        
         
+# interactieve modus uit als simulatie afgelopen
 plt.ioff()
 
-def main():
-    plt.show()
+
 if __name__ == "__main__":
-    main()
+    plt.show()
